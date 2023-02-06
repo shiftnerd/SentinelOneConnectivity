7c78e724d1b0317792d15a71a0bf877e4d7d1afc
Ben Climer <84200635+shiftnerd@users.noreply.github.com>
2023-02-06 00:03:43 -0600

    Update Test-SOConnectivity.ps1

--- Test-SOConnectivity.ps1
+++ Test-SOConnectivity.ps1
@@ -16,9 +16,15 @@ Function Test-SOConnectivity {
                 ValueFromPipelineByPropertyName = $true,
                 Position = 0
                 )]
-            [string[]]  $purl
+            [string[]]  $purl,
+            [Parameter(
+                Mandatory = $false,
+                ValueFromPipeline = $true,
+                ValueFromPipelineByPropertyName = $true,
+                Position = 1
+                )]
+            [string[]]  $sitetoken
         )
-     
         BEGIN {
             Resolve-DnsName -Name $purl
             Test-NetConnection -Port 443 -ComputerName $purl -InformationLevel Detailed
@@ -40,9 +46,31 @@ Function Test-SOConnectivity {
             }else { Write-output "SentinelCTL.exe check passed - continuing checks"}
             Write-outsput "The status of the SentinelAgent service is $SentinelAgent.Status"
             Write-outsput "The status of the SentinelStatic service is $SentinelStatic.Status"
-            & $SentinelCTL config server.mgmtServer
-            & $SentinelCTL config server.site
+            $MGMT = & $SentinelCTL config server.mgmtServer
+            $SITE = & $SentinelCTL config server.site
+            if (!$MGMT) {
+                Write-Error -Message "mgmtServer came back null or empty."
+                if(!$sitetoken) {
+                $sitetoken = Read-Host -Prompt 'Please enter site token so that we can attempt to re-connect.'
+                }
+                & $SentinelCTL bind {$purl | $sitetoken}
+            }
+            if (!$SITE) {
+                Write-Error -Message "Site came back null or empty."
+                if(!$sitetoken) {
+                    $sitetoken = Read-Host -Prompt 'Please enter site token so that we can attempt to re-connect.'
+                    }
+                    & $SentinelCTL bind {$purl | $sitetoken}
+            }
         }
      
-        END {}
+        END {
+            $ConnectionEvents = Get-WinEvent -LogName 'SentinelOne/Operational' | Where-Object {$_.Id -eq 5}
+            if(!$ConnectionEvents) {
+                Write-Host "No connection issue events found"
+        }else{
+            Write-Error -Message "Connection issue event logs found"
+            Write-Error -Message "Event logs found: $ConnectionEvents" -ErrorActionStop
+        }
     }
+}
