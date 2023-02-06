812c5be360605f99a39bac467012bc9f18510c21
Ben Climer <84200635+shiftnerd@users.noreply.github.com>
2023-02-06 00:55:15 -0600

    Update Test-SOConnectivity.ps1

--- Test-SOConnectivity.ps1
+++ Test-SOConnectivity.ps1
@@ -26,26 +26,22 @@ Function Test-SOConnectivity {
             [string[]]  $sitetoken
         )
         BEGIN {
-            Resolve-DnsName -Name $purl
-            Test-NetConnection -Port 443 -ComputerName $purl -InformationLevel Detailed
+            $purl | foreach-object {Resolve-DnsName -Name $_}
+            $purl | foreach-object {Test-NetConnection -Port 443 -ComputerName $_ -InformationLevel Detailed}
             $SentinelAgent = Get-Service SentinelAgent
+            $SAStatus = $SentinelAgent.status
             $SentinelStatic = Get-Service SentinelStaticEngine
+            $StaticStatus = $SentinelStatic.Status
             $SentinelFolder = Get-ChildItem "C:\Program Files\SentinelOne\Sentinel Agent*"
-            $SentinelCTL = $SentinelFolder.FullName + "\sentinelctl.exe"
+            $SentinelCTL = $SentinelFolder.FullName + "\SentinelCtl.exe"
         }
      
         PROCESS {
-            try {
-                Resolve-DnsName $purl -Server -ErrorAction Stop
-                }
-                catch {
-                  Write-Error -Message “DNS lookup for provided portal failed. It's always DNS.” -ErrorAction Stop
-                }
-            if (-not(Test-Path -Path $$SentinelCtl -PathType Leaf)) {
+            if (-not(Test-Path -Path $SentinelCtl -PathType Leaf)) {
                 Write-Error 'The sentinelctl.exe file does not exist, usually this means an uninstall failed or a corruption, SentinelSweeper is advised. Contact SentinelOne support.' -ErrorAction Stop
             }else { Write-output "SentinelCTL.exe check passed - continuing checks"}
-            Write-outsput "The status of the SentinelAgent service is $SentinelAgent.Status"
-            Write-outsput "The status of the SentinelStatic service is $SentinelStatic.Status"
+            Write-output "The status of the SentinelAgent service is $SAStatus"
+            Write-output "The status of the SentinelStatic service is $StaticStatus"
             $MGMT = & $SentinelCTL config server.mgmtServer
             $SITE = & $SentinelCTL config server.site
             if (!$MGMT) {
@@ -53,24 +49,31 @@ Function Test-SOConnectivity {
                 if(!$sitetoken) {
                 $sitetoken = Read-Host -Prompt 'Please enter site token so that we can attempt to re-connect.'
                 }
-                & $SentinelCTL bind {$purl | $sitetoken}
+                & $SentinelCTL bind {'$purl "|" $sitetoken'}
             }
             if (!$SITE) {
                 Write-Error -Message "Site came back null or empty."
                 if(!$sitetoken) {
                     $sitetoken = Read-Host -Prompt 'Please enter site token so that we can attempt to re-connect.'
                     }
-                    & $SentinelCTL bind {$purl | $sitetoken}
+                    
+                    & $SentinelCTL bind {'$purl "|" $sitetoken'}
             }
         }
      
         END {
-            $ConnectionEvents = Get-WinEvent -LogName 'SentinelOne/Operational' | Where-Object {$_.Id -eq 5}
+$filter = @{
+	Logname = 'SentinelOne/Operational'
+	ID = 5
+	StartTime =  [datetime]::Today.AddDays(-1)
+	EndTime = [datetime]::Today
+}
+Get-WinEvent -FilterHashtable $filter -ErrorAction SilentlyContinue
             if(!$ConnectionEvents) {
                 Write-Host "No connection issue events found"
         }else{
             Write-Error -Message "Connection issue event logs found"
-            Write-Error -Message "Event logs found: $ConnectionEvents" -ErrorActionStop
+            Write-Error -Message "Event logs found: $ConnectionEvents"
         }
     }
 }
